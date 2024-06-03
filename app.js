b4988b365ba5058e607488c69dab0eab8df8f625
Javier <jeduardoleon22@hotmail.com>
2024-06-03 14:48:20 -0600

    new api key

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const https = require('https');
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 const app = express();
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.listen(process.env.PORT || 3301, function () {console.log('Server Port 3301 On-Line')});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(process.env.PORT || 3301, function () { console.log('Server Port 3301 On-Line') });
 
 mailchimp.setConfig({
     apiKey: process.env.KEY,
@@ -20,11 +20,11 @@ app.route(['/', '/index', '/signup'])
     })
     .post(function (req, res) {
         const name = req.body.name;
-        const lastName =req.body.lastName;
+        const lastName = req.body.lastName;
         const email = req.body.email;
 
         const data = {
-            members: [ 
+            members: [
                 {
                     email_address: email,
                     status: 'subscribed',
@@ -36,14 +36,23 @@ app.route(['/', '/index', '/signup'])
             ]
         };
 
-        const run = async function () {
-            const chimpRes = await mailchimp.lists.batchListMembers(process.env.ID, data);
-            if (chimpRes.errors != []){
-                res.sendFile(`${__dirname}/succes.html`);
-            }
-            else {
-                res.sendFile(`${__dirname}/failure.html`);
-            }
+        const run = () => {
+            // const chimpRes = await mailchimp.lists.batchListMembers(process.env.ID, data);
+            // if (chimpRes.errors != []) {
+            //     res.sendFile(`${__dirname}/succes.html`);
+            // }
+            // else {
+            //     res.sendFile(`${__dirname}/failure.html`);
+            // }
+            mailchimp.lists.batchListMembers(process.env.ID, data)
+                .then((result) => {
+                    console.log('this response:', result)
+                    res.sendFile(`${__dirname}/succes.html`)
+                })
+                .catch((err) => {
+                    console.log('this error:', err)
+                    res.sendFile(`${__dirname}/failure.html`)
+                })
         };
         run();
     });
